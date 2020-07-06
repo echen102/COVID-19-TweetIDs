8ec48920ced8798046df1895916c1a406e5df9cb
CogSciHubVienna <53043710+CogSciHubVienna@users.noreply.github.com>
2020-07-06 12:05:39 +0200

    Update hydrate.py to include 2020-07 directory

--- hydrate.py
+++ hydrate.py
@@ -17,7 +17,7 @@ from twarc import Twarc
 from pathlib import Path
 
 twarc = Twarc()
-data_dirs = ['2020-01', '2020-02', '2020-03', '2020-04', '2020-05', '2020-06']
+data_dirs = ['2020-01', '2020-02', '2020-03', '2020-04', '2020-05', '2020-06', '2020-07']
 
 
 def main():
